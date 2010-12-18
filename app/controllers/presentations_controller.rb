e8c943547c272696c52e73c0ccd6eab0fb07176b
erihel <erihel@gmail.com>
2010-12-18 23:48:49 +0100

    dodanie uploadu plikow; genrerowanie obrazkow przy pomocy ghostscript; trzeba dodac jakis ladny progress, opcje oraz status zmieniac

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -6,7 +6,36 @@ class PresentationsController < ApplicationController
         respond_with @presentation
     end
 
-    def save
-
+    def create
+        @presentation = Presentation.new(params[:presentation])
+        if @presentation.save
+            puts @presentation
+            Presentation.save_file(@presentation)
+            fork do
+                pages = 0
+                page = 0
+                r_pages = Regexp.compile(/Processing pages 1 through (\d+)/)
+                dpi = "-r300"
+                res = '-dPDFFitPage -g750x1000'
+                filename = @presentation.filename.original_filename
+                dir = "public/presentations/#{@presentation.id}"
+                IO.popen("nice -n 15 gs -dSAFER -dBATCH -dNOPAUSE #{dpi} -dTextAlphaBits=4 -sDEVICE=png16m #{res} -sOutputFile='#{dir}/pres-%00d.png' #{dir}/#{filename}") do |readme|
+                readme.each do |line|
+                        if pages > 0 #&& line =~ /.+d+.+/
+                            page += 1
+                        end
+                        if r = r_pages.match(line)
+                            pages = r[1].to_f()
+                            @presentation.pages = pages
+                        end
+                    end
+                    if pages != page
+                        @presentation.status = "Error"
+                    end
+                end
+                @presentation.save
+            end
+        end
+        respond_with @presentation
     end
 end
--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -1,2 +1,9 @@
 class Presentation < ActiveRecord::Base
+    def self.save_file(upload)
+        file = upload.filename.original_filename
+        dir = "public/presentations/#{upload.id}/"
+        Dir.mkdir(dir) unless File.directory?(dir)
+        path = File.join(dir, file)
+        File.open(path, "wb") { |f| f.write(upload.filename.read) }
+    end
 end
