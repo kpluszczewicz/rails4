75d41ac8390181ed28c77307c3d3a3c33da6e3e6
Kamil Pluszczewicz <k.pluszczewicz@gmail.com>
2011-01-10 01:25:46 +0100

    Polerka stylów

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -2,7 +2,7 @@
 SimpleForm.setup do |config|
   # Components used by the form builder to generate a complete input. You can remove
   # any of them, change the order, or even add your own components to the stack.
-  # config.components = [ :placeholder, :label_input, :hint, :error ]
+  config.components = [ :placeholder, :label_input, :hint, :error ]
 
   # Default tag used on hints.
   # config.hint_tag = :span
@@ -29,7 +29,7 @@ SimpleForm.setup do |config|
   # config.error_notification_id = nil
 
   # You can wrap all inputs in a pre-defined tag.
-  # config.wrapper_tag = :div
+  config.wrapper_tag = :p
 
   # CSS class to add to all wrapper tags.
   # config.wrapper_class = :input
